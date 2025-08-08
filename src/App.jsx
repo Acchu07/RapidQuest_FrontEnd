a419d5f4cf3fe6a5314318b1c58869745f49c723
Acchu 07 <81754494+Acchu07@users.noreply.github.com>
2025-08-08 15:44:28 +0530

    Refactored code to enable side pane css
    CSS
    -Minimal CSS for buttons and visuals
    
    Functionality
    -Added in different view if accessed from phone

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import data from "./data"
-import { useState } from "react"
- /* 
+import data from "./data";
+import { useState, useEffect } from "react";
+/* 
   WhatsApp Web Like
   Process the data on the backend? -Ask about it later 
 */
@@ -8,69 +8,144 @@ import { useState } from "react"
 // ToDO last fix the types
 // Filter out the Unrequired data
 function App() {
+  const [dataStore, setDataStore] = useState();
+  const [isPhone, setIsPhone] = useState(false);
 
+  useEffect(() => {
+    setDataStore(data);
+    console.log(window.navigator.userAgent);
+    setIsPhone(window.navigator.userAgent.includes("Mobile"));
+  }, []);
+
+  if (!dataStore) {
+    return <p>Retrieving Data...</p>;
+  }
   return (
     <>
-    <Chat />
+      <Chat dataStore={dataStore} isPhone={isPhone} />
     </>
-  )
+  );
 }
 
-function Chat() {
+function Chat({ dataStore, isPhone }) {
   const [selectedWaId, setSelectedWaId] = useState(null);
 
-  const grouped = new Map();
+  const hashMapUserData = new Map(); // Rethink what the hashmap needs to store just identify and put in essentials only instead of all
 
-  data.forEach((msg) => {
-    if (!grouped.has(msg.wa_id)) {
-      grouped.set(msg.wa_id, {
+  dataStore.forEach((msg) => {
+    if (!hashMapUserData.has(msg.wa_id)) {
+      hashMapUserData.set(msg.wa_id, {
         name: msg.name,
         wa_id: msg.wa_id,
-        // Better array too many uneeded values maybe rethink what should be stored in db
-        messages: [] 
+        messages: [],
       });
     }
-    grouped.get(msg.wa_id).messages.push(msg);
+    hashMapUserData.get(msg.wa_id).messages.push(msg);
   });
 
   // Sort messages on backend? or sort before making a map
-  grouped.forEach((chat) => {
+  hashMapUserData.forEach((chat) => {
     chat.messages.sort(
       (a, b) => new Date(a.timestamp.$date) - new Date(b.timestamp.$date)
     );
   });
 
-  const chatList = Array.from(grouped.values());
+  const chatList = Array.from(hashMapUserData.values()); // Since code was changed this could be done different maybe directly use names from state
+  const chatMessages = hashMapUserData.get(selectedWaId); // Rethink what the hashmap needs to store just identify and put in essentials only instead of all
 
-  console.log(chatList)
+  // console.log(chatList)
+
+// Reptition with mobile and desktop possible to make just one?
+  if (isPhone && selectedWaId === null) {
+    return (
+      <div className="flex h-screen w-screen m-0.5 p-0.5 justify-center">
+        <div className="m-0.5 p-0.5">
+          {chatList.map((chat) => (
+            <div
+              className="block btn btn-ghost leading-10 "
+              key={chat.wa_id}
+              onClick={() => setSelectedWaId(chat.wa_id)}
+            >
+              <p>{chat.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  if (isPhone && selectedWaId !== null) {
+    return (
+      <>
+      <div className="flex h-screen w-screen m-0.5 p-0.5">
+        {selectedWaId === null ? (
+          <p>Chat Screen</p>
+        ) : (
+          <IndividualChat chatMessage={chatMessages} />
+        )}
+      </div>
+      <div>
+        {isPhone && selectedWaId !== null && (
+          <button onClick={() => setSelectedWaId(null)}>Go Back</button>
+        )}
+      </div>
+      </>
+    );
+  }
 
   return (
-    <div>
-      {chatList.map((chat) => (
-        <div
-          key={chat.wa_id}
-          onClick={() => setSelectedWaId(chat.wa_id)}
-        >
-          <p>Name: {chat.name}</p>
-          {selectedWaId === chat.wa_id &&
-            chat.messages.map((m) => (
-              <IndividualChat key={m._id} chatMessage={m} />
-            ))}
-        </div>
-      ))}
+    <div className="flex h-screen m-0.5 p-0.5">
+      <div className="m-0.5 p-0.5">
+        {chatList.map((chat) => (
+          <div
+            className="block btn btn-ghost leading-10 "
+            key={chat.wa_id}
+            onClick={() => setSelectedWaId(chat.wa_id)}
+          >
+            <p>{chat.name}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="divider divider-horizontal"></div>
+
+      <div className="flex-1">
+        {selectedWaId === null ? (
+          <p>Chat Screen</p>
+        ) : (
+          <IndividualChat chatMessage={chatMessages} />
+        )}
+      </div>
     </div>
   );
 }
 
 function IndividualChat({ chatMessage }) {
   return (
-    <div>
-      <p>Direction: {chatMessage.direction}</p>
-      <p>Status: {chatMessage.status}</p>
-      <p>{chatMessage.text}</p>
+    <div className="m-0.5 p-0.5">
+      {chatMessage.messages.map((msg) => (
+        // Using the wamid long string here - so it is required
+        <div
+          key={msg._id}
+          className={`chat ${
+            msg.direction === "outgoing" ? "chat-end" : "chat-start"
+          }`}
+        >
+          <div
+            className={`chat-bubble ${
+              msg.status === "read"
+                ? "chat-bubble-success"
+                : msg.status === "sent"
+                ? "chat-bubble-neutral"
+                : "chat-bubble-info"
+            }`}
+          >
+            {msg.text}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
 
-
-export default App
+export default App;
